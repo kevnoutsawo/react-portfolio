f9dfbf5633a0b5f03ac4cbd224c5f4fb1b4953af
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2022-05-28 12:59:45 +0100

    Implemented Home page styles

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,20 +1,53 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'
+import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
+import Logo from './Logo'
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['e', 'v', 'i', 'n']
+
+  //const jobArray = ['S', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p' ,'e', 'r']
+  const jobArray = "Software Developer".split('')
+
+  useEffect(() => {
+      let timeoutId = setTimeout(() => {
+          setLetterClass('text-animate-hover')
+      }, 4000)
+
+      return () => {
+          clearTimeout(timeoutId)
+      }
+  }, [])
+
   return (
     <div className="container home-page">
         <div className="text-zone">
-            <h1>Hi, <br /> I'm
-            <img src={LogoTitle} alt="developer" />
-            evin
+            <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
             <br />
-            Full Stack Software Developer
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters
+                letterClass={letterClass}
+                strArray={nameArray}
+                idx={15}
+            />
+            <br />
+            <AnimatedLetters
+                letterClass={letterClass}
+                strArray={jobArray}
+                idx={22}
+            />
             </h1>
             <h2>Frontend Developer / JavaScript Expert / Computer Engineering Graduate</h2>
             <Link to="/contact" className='flat-button'>CONTACT ME</Link>
         </div>
+        <Logo />
     </div>
   )
 }
